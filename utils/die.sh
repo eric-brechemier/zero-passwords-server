1560456647a4991b407f52150e58d612fdf63c6a
Eric Bréchemier <eric@bezen.org>
2016-08-09 15:43:54 +0200

    Add die() utility, to exit a script with given error message
    This utility was inspired by the 'die' function of Perl [1].
    It returns the same error code as the previous command,
    unless the previous error code was zero; a non-zero error code
    is always returned.
    
    [1] Perl Programming Documentation > Language reference > Functions > die
    http://perldoc.perl.org/functions/die.html

---
+++ utils/die.sh
@@ -0,0 +1,25 @@
+#!/bin/sh
+# Function: die(message)
+# Exit the terminal after printing an error message
+#
+# Parameter:
+#   $1 - string, the error message
+#
+# Standard Error:
+#   The error message is printed to standard error.
+#
+# Status:
+#   The status code of the previous command is used as status code for this
+#   utility, unless it is 0; die() always returns a non-zero status code.
+die()
+{
+  die_code=$?
+  die_message="$1"
+  echo "$die_message" 1>&2
+  if test $die_code = 0
+  then
+    exit 1
+  else
+    exit $die_code
+  fi
+}
